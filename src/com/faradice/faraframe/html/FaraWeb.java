616392f7b3c3a748448abe78e30d9dbf95a0cc53
Ragnar Þór Valdimarsson <ragnarthv@islandsbanki.is>
2019-01-06 23:20:59 +0000

    FaraFame host

--- src/com/faradice/faraframe/html/FaraWeb.java
+++ src/com/faradice/faraframe/html/FaraWeb.java
@@ -10,23 +10,56 @@ import com.faradice.faraUtil.FaraFiles;
 import com.faradice.faraframe.properties.BasicPropertyItem;
 import com.faradice.faranet.FaraHtml;
 
-import sun.awt.image.URLImageSource;
-
 public class FaraWeb {
 	public static String begin() {
-		String hd = "<html><head>";
-		hd+= loadFromCP("Header.html").replace("$(Title)", "Faradice");
-		hd+="<style>";
-		hd+= loadFromCP("css/Common.css");
-		hd+= "";
-		hd+="</style>";
+		String hd = "<html>";
+		hd+="<head>";
+		hd+= header("Register user");
+		hd+=fontAwesome();
+		hd+= css("Common.css");
 		hd+= "</head>";
 		hd+= "<body>";
 		hd+="<div class='page-wrap'>";
-	    hd+= loadFromCP("Top.html").replace("$(Name)", "Faradice");
+	    hd+= top("Faradice");
 		return hd;
 	}
+	
+	public static String header(String title) {
+		String h = "";
+		h+="<title>"+title+"</title>";
+		h+="<meta charset='utf-8'>";
+		h+="<meta name='viewport' content='width=device-width, initial-scale=1'>";
+		return h;
+	}
 
+	/*
+	   <div class='Header' style=text-align:center>
+	      <span class='fas fa-charging-station' style=float:left></span>
+	      <span style=''>$(Name)</span>
+	      <span id='menu' class='fas fa-bars menu' style=float:right></span>
+	   </div>
+	   */
+	   
+	public static String top(String caption) {
+		String h = "";
+		h+="<div class='Header' style='text-align:center'>";
+		h+="<span class='fas fa-charging-station' style='float:left'></span>";
+        h+="<span style=''>"+caption+"</span>";
+		h+="<span id='menu' class='fas fa-bars menu' style='float:right'></span>";
+		h+="</div>";
+		return h;
+	}
+	
+	public static String fontAwesome() {
+		String fa = "<link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.6.3/css/all.css' integrity='sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/' crossorigin='anonymous'>";
+		return fa;
+	}
+	
+	public static String css(String resource) {
+		String css = "<link rel='stylesheet' type='text/css' href='http://www.faradice.com/resource/css/"+resource+"' />";
+		return css;
+	}
+	
 	public static String end() {
 		String end = "</div>";
 		end += "<footer class='site-footer'><a href='http://www.faradice.com'>Visit Faradice Home</a></footer>";
@@ -66,9 +99,9 @@ public class FaraWeb {
 		return form;
 	}
 	
-	public static String beginCard() {
+	public static String beginCard(int width) {
 		StringBuffer sb = new StringBuffer();
-		sb.append("<div class='card' style='width:300;margin:10 auto;'>");
+		sb.append("<div class='card' style='width:"+width+";margin:10 auto;'>");
 		sb.append("<div class='container'>");
 		sb.append("<div style='margin:1em;font-size:120%'>Register User</div>");
 		sb.append("<div style='margin:1em'>");
